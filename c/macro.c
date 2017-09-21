b6fc67f0c2f7ee6de588f1f5a8fd0a2a131c6157
libo-ps <libo-ps@360.cn>
2017-09-21 12:25:11 +0800

    宏定义详解

--- c/macro.c
+++ c/macro.c
@@ -6,6 +6,12 @@
 宏定义两种情况：结尾都没有分号.
 1. 直接定义一个常量，注意不是用=号赋值;
 2. 定义一个函数式宏，可以用\作宏的延续，解决单行写不完一个宏的问题.注意：在使用该宏的时候，传入的字符串是不需要引号的.
+
+预处理器运算符
+C 预处理器提供了下列的运算符来帮助您创建宏：
+宏延续运算符（\）
+字符串常量化运算符（#）
+标记粘贴运算符（##）
 */
 
 #define WIDTH 10
@@ -13,8 +19,15 @@
 #define sayHello(name, where) \
     printf("Jim say Hello to " #name ", and tell him he will go " #where "\n")
 
+#define token(num) printf("the token" #num " = %d\n", token##num)  //相当于把token和num的值连接到一起作为一个变量名token12
+
 
 int main(){
     printf("Width is : %d\n", WIDTH);
+
     sayHello(Amy, Great Wall);
+    
+    int token12 = 100;
+    token(12);
+
 }
