6580ae50ea0adfbf8f57222a79ea109c2d122c87
libo-ps <libo-ps@360.cn>
2018-07-06 08:24:23 +0800

    cpp base note

--- cpp/start.cpp
+++ cpp/start.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
 using namespace std;
+
+struct Books
+{
+    char title[5];
+    int book_id;
+};
+
 int main()
 {
-    cout << "Hello,world!" << endl;
+    Books book;
+    strcpy(book.title,"haha");
+    clog << "name is :"<< book.title  << endl;
+
+    char data[100];
+
+    ofstream of;
+    of.open("xx.log",ios::app|ios::in);
+    of << "xot\n";
+    of.close();
+
+    ifstream infile;
+    infile.open("xx.log"); 
+    infile.seekg(3);
+    infile >> data;
+    cout << data << endl;
+    infile.close();
     return 0;
+
 }
